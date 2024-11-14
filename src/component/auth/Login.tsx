a0c9f30ea8a7405d5c6a7cf88c114e10bd4c9861
avraham <2721avi@gmail.com>
2024-11-14 09:59:56 +0200

    Login done

--- src/component/auth/Login.tsx
+++ src/component/auth/Login.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/store/store";
+import { fetchLogin } from "../../redux/fetchs/fetches";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(fetchLogin({ email, password }));
+  };
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleLogin} className="auth">
+      <input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
