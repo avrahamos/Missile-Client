0bae14b42d5df9d80d73718e4838611c6d0fa305
avraham <2721avi@gmail.com>
2024-11-14 18:10:27 +0200

    done time

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,18 @@ import Login from "./component/auth/Login";
 import Dashboard from "./component/page/Dashboard";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-
 function App() {
   const token = localStorage.getItem("authorization");
 
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={token ? <Navigate to="/dashboard" /> : <Login />}
-        />
+        <Route path="/" element={<Navigate to={"/login"} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
           path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/" />}
+          element={token ? <Dashboard /> : <Navigate to="/register" />}
         />
       </Routes>
     </>
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,17 +3,16 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 export const connectSocket = (token: string): Socket => {
-
   socket = io("http://localhost:5678", {
-    auth: { token }, 
+    auth: { token },
   });
 
   socket.on("connect", () => {
-    console.log("Connected WebSocket server");
+    console.log("Connected to WebSocket server");
   });
 
   socket.on("disconnect", () => {
-    console.log("Disconnected WebSocket server");
+    console.log("Disconnected from WebSocket server");
   });
 
   socket.on("error", (error) => {
